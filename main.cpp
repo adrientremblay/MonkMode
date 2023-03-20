7185792103d04ef9c702658a1888ce6d47a9f75e
Adrien Tremblay <adrien@adrientremblay.com>
2023-03-19 21:35:49 -0400

    Two threads running at the same time

--- main.cpp
+++ main.cpp
@@ -8,6 +8,8 @@
 #include <ctime>
 #include <fstream>
 #include <string>
+#include <ncurses.h>
+#include <pthread.h>
 
 #define SAVE_FILE_NAME "monk_save.txt"
 
@@ -32,20 +34,25 @@ void save() {
     }
 }
 
-int main() {
-    // Load Save
-    std::ifstream inFile(SAVE_FILE_NAME);
-    if (inFile.is_open()) {
-        inFile >> monk.name;
-    } else {
-        character_creation();
-        save();
+void* input_thread_func(void* arg){
+    initscr(); // Initialize ncurses
+    noecho(); // Disable automatic echoing of input characters
+    keypad(stdscr, true); // Enable function keys (e.g. arrow keys)
+
+    char c;
+    while ((c = getchar()) != EOF) {
+        std::cout << "Input character: " << c << std::endl;
     }
 
+    return NULL;
+}
+
+void* game_thread_func(void* arg){
     // Main Loop
     system("clear");
     std::time_t now;
     unsigned int save_counter = 0;
+
     while (true) {
         system("clear");
         now = std::time(nullptr);
@@ -63,6 +70,31 @@ int main() {
 
         std::this_thread::sleep_for(std::chrono::seconds(1));
     }
+
+    return NULL;
+}
+
+int main() {
+    // Load Save
+    std::ifstream inFile(SAVE_FILE_NAME);
+    if (inFile.is_open()) {
+        inFile >> monk.name;
+    } else {
+        character_creation();
+        save();
+    }
+
+    // Setting up threads
+    pthread_t input_thread;
+    pthread_create(&input_thread, NULL, input_thread_func, nullptr);
+
+    pthread_t game_thread;
+    pthread_create(&game_thread, NULL, game_thread_func, nullptr);
+
+    pthread_join(input_thread, NULL);
+    pthread_join(game_thread, NULL);
+
+    std::cout << "penis" << std::endl;
 
     return 0;
 }
