ef40be1297a67cddc4429e1b528b1638d65d9846
Adrien Tremblay <adrien@adrientremblay.com>
2023-03-20 22:13:26 -0400

    Monk kneels if sanity is low

--- main.cpp
+++ main.cpp
@@ -15,46 +15,13 @@
 #include <boost/archive/text_oarchive.hpp>
 #include <boost/archive/text_iarchive.hpp>
 #include <boost/serialization/vector.hpp>
+#include "monks.h"
 
 #define SAVE_FILE_NAME "monk_save.txt"
 
 WINDOW* info_win;
 WINDOW* monk_win;
 WINDOW* vices_win;
-
-const char* monk_img = "         ^?YYY5PP5?       \n"
-                       "        ^BP#5YYPP5&J      \n"
-                       "        .#B5YYYPGG#&?     \n"
-                       "         !@BYYYGBB&#&Y.   \n"
-                       "         7@@&GPPG#&#&#P:  \n"
-                       "        ?@&&&#B&#GP55BGG: \n"
-                       "        J@#&@&GPYY5YJY5#J \n"
-                       "      .J##G#BPYY5YGBBP5PB5\n"
-                       "     ^BBGPPB5YY5GG&@&GPBG!\n"
-                       "     :##BG#BB#&@@@@G5YYP: \n"
-                       "    ~GBBBB#&#&@@#BBG5YYB: \n"
-                       "   :#&BBYYBP55B#5YYYYYB#. \n"
-                       "   :###PYYGBB#PB5YY5PB#7  \n"
-                       "    G&G5PY55G#GGBPPG&G.   \n"
-                       "    ^5GGBBBBBGGBB#&&@&!   \n"
-                       "      .^&##&&@@&&&&&#&#^  \n"
-                       "       7&PGG&&&GBG&##@#~  \n"
-                       "       5BPGP#&&GGG&B#&#7  \n"
-                       "      .#GGGGB&P5PG#G&BBP  \n"
-                       "      !@GGGGBGYY5PGB@GG#. \n"
-                       "      G@55GP#GYYY5G&@P5@~ \n"
-                       "     :&@5YY5##YYYP#@BYP@? \n"
-                       "     ~@@PYY5&#YYYB@&5YP@Y \n"
-                       "     7@@PYYP&#5Y5@&GPYP@5 \n"
-                       "     5@#YYYPB#5YB#PGGPP@P \n"
-                       "     B@GYYYPGGPY5YYYGGB@B \n"
-                       "    :&@PYYYP#GPYYYYY5GB@#.\n"
-                       "    7@@PYYYP&BG5YYYYYGB@&:\n"
-                       "   ^&@@PYYYG@#GPYYYYYGB@@~\n"
-                       "  ^#@@@PYYYG@&GGG5Y5PG#@@!\n"
-                       "  JB@@@#5YYG&@&GGGGGGG&@@J\n"
-                       "    YB#BB#BB&@@@&&&&&&@Y~^\n"
-                       "      . ?#&&@@@&&&##BGP:  ";
 
 struct Vice {
     std::string name;
@@ -161,7 +128,7 @@ void draw_screen() {
     wrefresh(vices_win);
 
     // Monk Window
-    mvwprintw(monk_win, 1, 0, monk_img);
+    mvwprintw(monk_win, 1, 0, sanity >= 50 ? monk_img : kneeling_monk);
     box(monk_win, 0, 0);
     mvwprintw(monk_win, 0, 2, "MONK");
 
---
+++ monks.h
@@ -0,0 +1,60 @@
+//
+// Created by adrien on 20/03/23.
+//
+
+#ifndef MONKMODE_MONKS_H
+#define MONKMODE_MONKS_H
+
+const char* monk_img = "         ^?YYY5PP5?       \n"
+                       "        ^BP#5YYPP5&J      \n"
+                       "        .#B5YYYPGG#&?     \n"
+                       "         !@BYYYGBB&#&Y.   \n"
+                       "         7@@&GPPG#&#&#P:  \n"
+                       "        ?@&&&#B&#GP55BGG: \n"
+                       "        J@#&@&GPYY5YJY5#J \n"
+                       "      .J##G#BPYY5YGBBP5PB5\n"
+                       "     ^BBGPPB5YY5GG&@&GPBG!\n"
+                       "     :##BG#BB#&@@@@G5YYP: \n"
+                       "    ~GBBBB#&#&@@#BBG5YYB: \n"
+                       "   :#&BBYYBP55B#5YYYYYB#. \n"
+                       "   :###PYYGBB#PB5YY5PB#7  \n"
+                       "    G&G5PY55G#GGBPPG&G.   \n"
+                       "    ^5GGBBBBBGGBB#&&@&!   \n"
+                       "      .^&##&&@@&&&&&#&#^  \n"
+                       "       7&PGG&&&GBG&##@#~  \n"
+                       "       5BPGP#&&GGG&B#&#7  \n"
+                       "      .#GGGGB&P5PG#G&BBP  \n"
+                       "      !@GGGGBGYY5PGB@GG#. \n"
+                       "      G@55GP#GYYY5G&@P5@~ \n"
+                       "     :&@5YY5##YYYP#@BYP@? \n"
+                       "     ~@@PYY5&#YYYB@&5YP@Y \n"
+                       "     7@@PYYP&#5Y5@&GPYP@5 \n"
+                       "     5@#YYYPB#5YB#PGGPP@P \n"
+                       "     B@GYYYPGGPY5YYYGGB@B \n"
+                       "    :&@PYYYP#GPYYYYY5GB@#.\n"
+                       "    7@@PYYYP&BG5YYYYYGB@&:\n"
+                       "   ^&@@PYYYG@#GPYYYYYGB@@~\n"
+                       "  ^#@@@PYYYG@&GGG5Y5PG#@@!\n"
+                       "  JB@@@#5YYG&@&GGGGGGG&@@J\n"
+                       "    YB#BB#BB&@@@&&&&&&@Y~^\n"
+                       "      . ?#&&@@@&&&##BGP:  ";
+
+const char* kneeling_monk = "                         \n"
+                            "                         \n"
+                            "    :                    \n"
+                            "   .BB7.                 \n"
+                            "   5@@@#55?:             \n"
+                            "  7@@@@@@@@#5^           \n"
+                            "  !YPB##@@@@@#:          \n"
+                            "       ^&@@@@@5          \n"
+                            "       !@@@@@@B          \n"
+                            "      .?@@@@@@5          \n"
+                            "    ^G&&@@@@@@B^         \n"
+                            "    .!!Y@@@@@@@G         \n"
+                            "       ^@#@@@@@&.        \n"
+                            "       ~@B@@@@@@~        \n"
+                            "       ~&B@@@@@@Y::^7.   \n"
+                            "       ^&#@@@@@@@&&&@B7: \n"
+                            "       ~@@@@@@@@@@@&@@@P^";
+
+#endif //MONKMODE_MONKS_H
